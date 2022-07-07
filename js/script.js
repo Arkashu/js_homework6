d6221f8faf77235853bead837a09911ad3a01199
Arkashu <kopeikaarkashu@gmail.com>
2022-07-07 23:50:09 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+//1
+const num = Boolean(Number('10')) + 1;
+// Вернёт 2
+
+//2
+const num2 = 'sub ' + Number(false);
+// Вернёт 'sub 0'
+
+//3
+const num3 = 16  *   '     91    '
+// Вернёт 1456
+
+//4
+const num4 = true-70;
+// Вернёт -69
+
+//5
+const num5 = Number(1 + String(1)) + 1;
+// Вернёт 12
